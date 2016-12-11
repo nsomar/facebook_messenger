c5ee0dd0a22375bfb5ed39cc92a8e12ebae2b994
Yuriy Plugatariov <plugatariov@gmail.com>
2016-12-12 01:09:46 +0200

    add postback data type

--- lib/facebook_messenger_models.ex
+++ lib/facebook_messenger_models.ex
@@ -14,6 +14,19 @@ defmodule FacebookMessenger.Message do
   }
 end
 
+defmodule FacebookMessenger.Postback do
+  @moduledoc """
+    Facebook postback structure
+  """
+
+  @derive [Poison.Encoder]
+  defstruct [:payload]
+
+  @type t :: %FacebookMessenger.Postback{
+    payload: String.t
+  }
+end
+
 defmodule FacebookMessenger.User do
   @moduledoc """
   Facebook user structure
