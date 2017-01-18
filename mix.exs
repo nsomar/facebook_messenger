1a932d4b92e954ad5766bc74c3a84430cedb19af
Blaž Hrastnik <blaz.hrast@gmail.com>
2017-01-18 20:40:09 +0900

    Allow both poison 2.1.x and 3.x

--- mix.exs
+++ mix.exs
@@ -40,9 +40,9 @@ defmodule FacebookMessenger.Mixfile do
          {:earmark, "~> 0.1", only: :docs}]
 
     if Mix.env == :test do
-      [{:coverex, "~> 1.4.8", only: :test}, {:poison, "~> 2.1.0", override: true} | d]
+      [{:coverex, "~> 1.4.8", only: :test}, {:poison, "~> 2.1.0 or ~> 3.0", override: true} | d]
     else
-      [{:poison, "~> 2.1.0"} | d]
+      [{:poison, "~> 2.1.0 or ~> 3.0"} | d]
     end
   end
 
