1d689a5e09a21ee4e605080ac79ca907b16f2f9f
Arjan Scherpenisse <arjan@scherpenisse.net>
2017-07-03 14:02:12 +0200

    Add unit tests for sender action, templates, attachment sending

--- test/message_sender_test.exs
+++ test/message_sender_test.exs
@@ -6,24 +6,9 @@ defmodule TestBotOne.MessageSenderTest do
     assert FacebookMessenger.Sender.url == "https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_TOKEN"
   end
 
-  test "creates a correct image payload" do
-    assert FacebookMessenger.Sender.image_payload(1055439761215256, "sample.com/some_image.png") ==
-      %{
-        recipient: %{id: 1055439761215256},
-        message: %{
-           attachment: %{type: "image", payload: %{url: "sample.com/some_image.png"}}
-        }
-       }
-  end
-
-  test "creates a correct text payload" do
-    assert FacebookMessenger.Sender.text_payload(1055439761215256, "Hello") ==
-    %{message: %{text: "Hello"}, recipient: %{id: 1055439761215256}}
-  end
-
-  test "converts hash to json"do
-    assert FacebookMessenger.Sender.to_json(%{test_key: "test_value"}) ==
-    "{\"test_key\":\"test_value\"}"
+  test "sends sender_action" do
+    FacebookMessenger.Sender.sender_action(1055439761215256, :typing_on)
+    assert_received %{body: "{\"sender_action\":\"typing_on\",\"recipient\":{\"id\":1055439761215256}}", url: "https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_TOKEN"}
   end
 
   test "sends correct text message" do
@@ -35,4 +20,24 @@ defmodule TestBotOne.MessageSenderTest do
     FacebookMessenger.Sender.send_image(1055439761215256, "sample.com/some_image.png")
     assert_received %{body: "{\"recipient\":{\"id\":1055439761215256},\"message\":{\"attachment\":{\"type\":\"image\",\"payload\":{\"url\":\"sample.com/some_image.png\"}}}}", url: "https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_TOKEN"}
   end
+
+  test "sends correct template" do
+    template = %{
+      "template_type" => "button",
+      "text" => "What do you want to do next?",
+      "buttons" => [%{
+                       "type" => "web_url",
+                       "url" => "https://petersapparel.parseapp.com",
+                       "title" => "Show Website"
+                    }
+                   ]}
+    FacebookMessenger.Sender.send_template(1055439761215256, template)
+
+    body = "{\"recipient\":{\"id\":1055439761215256},\"message\":{\"attachment\":{\"type\":\"template\",\"payload\":#{Poison.encode!(template)}}}"
+    %{body: body} = assert_received %{url: "https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_TOKEN"}
+
+    payload = Poison.decode!(body)["message"]["attachment"]["payload"]
+    assert payload == template
+  end
+
 end
